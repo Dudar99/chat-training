a9fcfaca06aa728b8f8c544227c80ee5a16ddfb0
dudar99 <dudaryourko@egmail.com>
2018-12-30 16:21:35 +0200

    rewrite routes for database and offset storage

--- application/consumer/routes/routes.py
+++ application/consumer/routes/routes.py
@@ -3,26 +3,25 @@ from sanic.response import text, json
 from consumer.db_services import RedisManager, ZookeeperManager, CassandraManager
 from consumer.db_services import PGManager
 from consumer.models import Message
+from consumer.config import Configs
 
 
 async def get_messages_count(request):
-    return json({'row_count': await PGManager.table_row_count(Message)})
+    if Configs['DATA_STORAGE'] == 'POSTGRES':
+        response = json({'row_count': await PGManager.table_row_count(Message)}), 200
+    else:
+        response = json({'row_count': await CassandraManager.select_count()}), 200
+    return response
 
 
 async def get_last_offset(request):
-    return json({'kafka_offset': await RedisManager.get_value('kafka')})
-
-
-async def get_last_offset_zk(request):
-    return json({'kafka_offset': await ZookeeperManager.get()})
-
-
-async def get_messages_count_cs(request):
-    return json({"cassandra_row_count": await CassandraManager.select_count()})
+    if Configs['OFFSET_STORAGE'] == 'REDIS':
+        response = json({'kafka_offset': await RedisManager.get_value('kafka')}), 200
+    else:
+        response = json({'kafka_offset': await ZookeeperManager.get()}), 200
+    return response
 
 
 def add_routes(app):
-    app.add_route(get_last_offset_zk, '/offset_zk', methods=['GET'])
-    app.add_route(get_messages_count_cs, '/count_cs', methods=['GET'])
     app.add_route(get_messages_count, '/count', methods=['GET'])
     app.add_route(get_last_offset, '/offset', methods=['GET'])
